0e2dd097dfbc2fbe1f1e6a5548c1b9f509cab0fe
Pandalism <f.diaz-maroto@hotmail.com>
2020-01-19 00:26:30 +0000

    finished french section

--- hello/hello.go
+++ hello/hello.go
@@ -3,16 +3,35 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 const surpriseSufix = "!"
+const defaultUser = "World"
+const langES = "Spanish"
+const langFR = "French"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 
 	if name == "" {
-		name = "World"
+		name = defaultUser
 	}
-	return englishHelloPrefix + name + surpriseSufix
+
+	return greetingPrefix(language) + name + surpriseSufix
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case langES:
+		prefix = spanishHelloPrefix
+	case langFR:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,15 +12,29 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris!"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello to world if no people mentioned", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("test that spanish is used as an input", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("test where french is used as an input", func(t *testing.T) {
+		got := Hello("Marc", "French")
+		want := "Bonjour, Marc!"
 
 		assertCorrectMessage(t, got, want)
 	})
